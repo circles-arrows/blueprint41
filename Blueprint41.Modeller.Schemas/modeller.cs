21af5662140a5721f058804c39c7c27b11bd2056
master168 <32819844+master168@users.noreply.github.com>
2018-01-29 16:17:44 +0800

    Partial fix for save mapping

--- Blueprint41.Modeller.Schemas/modeller.cs
+++ Blueprint41.Modeller.Schemas/modeller.cs
@@ -21,58 +21,24 @@ namespace Blueprint41.Modeller.Schemas
             entitiesByGuid.TryGetValue(new Guid(guid), out found);
             return found;
         }
+
         private Dictionary<Guid, entity> entitiesByGuid = null;
 
-        public void UpdateEntityGuid(entity entity, Guid newId)
+        public void UpdateEntityGuid(entity entity, Guid? newId)
         {
-            Guid oldId = Guid.Parse(entity.guid);
-
-            // TODO: Fix this to more robust real refactoring solution....
-            // TODO: Why? because this can update conflicting guids when it shouldn't...
-            GenericQuickHackToRefactorGuidsAllOverThePlace(newId, oldId);
+            entity.mappingGuid = newId?.ToString();
         }
-        public void UpdateRelationshipGuid(relationship relationship, Guid newId)
+        public void UpdateRelationshipGuid(relationship relationship, Guid? newId)
         {
-            Guid oldId = Guid.Parse(relationship.guid);
-
-            // TODO: Fix this to more robust real refactoring solution....
-            // TODO: Why? because this can update conflicting guids when it shouldn't...
-            GenericQuickHackToRefactorGuidsAllOverThePlace(newId, oldId);
+            relationship.mappingGuid = newId?.ToString();
         }
-        public void UpdatePrimitivePropertyGuid(entity entity, primitive prop, Guid newId)
+        public void UpdatePrimitivePropertyGuid(entity entity, primitive prop, Guid? newId)
         {
-            Guid oldId = Guid.Parse(prop.guid);
-
-            // TODO: Fix this to more robust real refactoring solution....
-            // TODO: Why? because this can update conflicting guids when it shouldn't...
-            GenericQuickHackToRefactorGuidsAllOverThePlace(newId, oldId);
+            prop.mappingGuid = newId?.ToString();
         }
-        public void UpdateRecordGuid(entity entity, record prop, Guid newId)
+        public void UpdateRecordGuid(entity entity, record prop, Guid? newId)
         {
-            Guid oldId = Guid.Parse(prop.guid);
-
-            // TODO: Fix this to more robust real refactoring solution....
-            // TODO: Why? because this can update conflicting guids when it shouldn't...
-            GenericQuickHackToRefactorGuidsAllOverThePlace(newId, oldId);
-        }
-
-        private void GenericQuickHackToRefactorGuidsAllOverThePlace(Guid newId, Guid oldId)
-        {
-            foreach (XElement element in Untyped.Document.Descendants())
-            {
-                foreach (XAttribute attr in element.Attributes())
-                {
-                    if (attr.Value != null)
-                    {
-                        Guid actual;
-                        if (Guid.TryParse(attr.Value, out actual))
-                        {
-                            if (actual == oldId)
-                                attr.Value = newId.ToString();
-                        }
-                    }
-                }
-            }
+            prop.mappingGuid = newId?.ToString();
         }
     }
 }
