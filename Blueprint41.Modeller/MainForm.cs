fc1997cda30b7b4a4fd523e47d151d9a8117a024
xirqlz <31648959+xirqlz@users.noreply.github.com>
2018-05-30 17:31:23 +0800

    added menu items removed toolstrip names and maintain icons Show/Hide properties pane

--- Blueprint41.Modeller/MainForm.cs
+++ Blueprint41.Modeller/MainForm.cs
@@ -66,8 +66,6 @@ namespace Blueprint41.Modeller
                 toolStripDropDownButton3.Visible = false;
 
             Initialize();
-
-
         }
 
         private void Initialize()
@@ -82,6 +80,7 @@ namespace Blueprint41.Modeller
         }
 
         int idcounter = 0;
+
         internal string GetNewId(NodeTypeEntry nte)
         {
             string ret = nte.DefaultLabel + idcounter++;
@@ -457,7 +456,7 @@ namespace Blueprint41.Modeller
 
         private void splitContainer1_SizeChanged(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(string.Format("Width:{0}", splitContainer1.Panel2.Width));
+            System.Diagnostics.Debug.WriteLine(string.Format("Width:{0}", splitContainer.Panel2.Width));
         }
 
         private void btnSave_Click(object sender, EventArgs e)
@@ -472,6 +471,7 @@ namespace Blueprint41.Modeller
         private void btnShowLabels_Click(object sender, EventArgs e)
         {
             showLabels = btnShowLabels.Checked;
+            showLabelsToolStripMenuItem.Checked = btnShowLabels.Checked;
             Model.ShowRelationshipLabels = btnShowLabels.Checked;
         }
 
@@ -551,6 +551,7 @@ namespace Blueprint41.Modeller
         private void btnShowInheritedRelationships_Click(object sender, EventArgs e)
         {
             showInherited = btnShowInheritedRelationships.Checked;
+            showInheritedRelationshipsToolStripMenuItem.Checked = btnShowInheritedRelationships.Checked;
             Model.ShowInheritedRelationships = btnShowInheritedRelationships.Checked;
         }
 
@@ -656,6 +657,53 @@ namespace Blueprint41.Modeller
             else
                 DatastoreModelComparer.Instance.GenerateUpgradeScript(Model.Xml, StoragePath);
 
+        }
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            splitContainer.Panel2Collapsed = !propertiesToolStripMenuItem.Checked;
+            ShowHideToolStripMenu(splitContainer.Panel2Collapsed);
+        }
+
+        private void ShowHideToolStripMenu(bool panel2Collapsed)
+        {
+            if (panel2Collapsed)
+            {
+                ToolStripMenuItem propertiesItem = new ToolStripMenuItem("Properties");
+                propertiesItem.Font = new Font(FontFamily.GenericSansSerif, 8.25f);
+                propertiesItem.Click += PropertiesItem_Click;
+                toolStripRight.Items.Add(propertiesItem);
+            }
+            else
+            {
+                if (toolStripRight.Items[0] is ToolStripMenuItem menuItem)
+                    menuItem.Click -= PropertiesItem_Click;
+
+                toolStripRight.Items.Clear();
+            }
+        }
+
+        private void PropertiesItem_Click(object sender, EventArgs e)
+        {
+            propertiesToolStripMenuItem.Checked = !propertiesToolStripMenuItem.Checked;
+            propertiesToolStripMenuItem_Click(sender, e);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void showLabelsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnShowLabels.Checked = !btnShowLabels.Checked;
+            btnShowLabels_Click(sender, e);
+        }
+
+        private void showInheritedRelationshipsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnShowInheritedRelationships.Checked = !btnShowInheritedRelationships.Checked;
+            btnShowInheritedRelationships_Click(sender, e);
         }
     }
 }
