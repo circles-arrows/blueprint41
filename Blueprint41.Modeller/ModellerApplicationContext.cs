3f81813f564cdebf078442c3457e7657cb10e660
circle-arrows <31648959+xirqlz@users.noreply.github.com>
2019-02-20 16:20:16 +0800

    Removed magic strings, added url to the app config settings

--- Blueprint41.Modeller/ModellerApplicationContext.cs
+++ Blueprint41.Modeller/ModellerApplicationContext.cs
@@ -1,9 +1,11 @@
 ﻿using Blueprint41.Licensing.Connector;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,29 +21,62 @@ namespace Blueprint41.Modeller
             loader = new Loader();
             loader.Show();
 
-            string connectorFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Blueprint41.Licensing.Client.dll");
-            using (WebClient client = new WebClient())
-            {
-                client.DownloadFileAsync(new Uri("http://localhost:14564/api/License/Connector"), connectorFileName);
-                client.DownloadFileCompleted += Client_DownloadFileCompleted;
-            }
+            DownloadConnectorClient();
         }
 
-        private async void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            await ConnectorLoader.GetConnectorClient().VerifyLicense();
-
-            loader.Close();
-
-            MainForm main = new MainForm();
-            main.FormClosed += OnFormClosed;
-            main.Show();
+            VerifyLicense();
         }
 
         private void OnFormClosed(object sender, EventArgs e)
         {
             if (Application.OpenForms.Count == 0)
                 ExitThread();
+        }
+
+        private void ShowMainForm()
+        {
+            MainForm main = new MainForm();
+            main.FormClosed += OnFormClosed;
+            main.Show();
+        }
+
+        private async void VerifyLicense()
+        {
+            ConnectorLoader connectorClientLoader = ConnectorLoader.GetConnectorClient();
+
+            try
+            {
+                // wrapping it to try/catch will catch any exception from the verifylicense method
+                if (connectorClientLoader != null)
+                    await connectorClientLoader.VerifyLicense();
+            }
+            catch { }
+
+            loader.Close();
+            ShowMainForm();
+        }
+
+        private async void DownloadConnectorClient()
+        {
+            // Checking the server first eliminates the connector client dll to be created by the code below 
+            // when the server is offline.
+
+            bool isOnline = await UriConfig.CheckServerIsOnline();
+
+            if (isOnline == false)
+            {
+                VerifyLicense();
+                return;
+            }
+
+            using (WebClient client = new WebClient())
+            {
+                string connectorFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Blueprint41.Licensing.Client.dll");
+                client.DownloadFileAsync(UriConfig.ConnectorUri, connectorFileName);
+                client.DownloadFileCompleted += Client_DownloadFileCompleted;
+            }
         }
     }
 }
---
+++ Blueprint41.Modeller/UriConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blueprint41.Modeller
+{
+    public static class UriConfig
+    {
+        public static string LicensingUrl => ConfigurationManager.AppSettings.Get("LicensingUrl");
+        public static Uri ConnectorUri => new Uri(new Uri(LicensingUrl), @"api/license/connector");
+        public static Uri PingUri => new Uri(new Uri(LicensingUrl), @"api/license/ping");
+
+        internal static async Task<bool> CheckServerIsOnline()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(PingUri);
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return false;
+            }
+        }
+    }
+}
