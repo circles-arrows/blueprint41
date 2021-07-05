a66261cd3e9674f6c6888f8194ce85275884e14f
circles-arrows <31648959+circles-arrows@users.noreply.github.com>
2021-07-05 23:56:17 +0800

    released version 1.0.18

--- Blueprint41.Neo4jDriver.v3/Properties/AssemblyInfo.cs
+++ Blueprint41.Neo4jDriver.v3/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
-﻿using Blueprint41.Core;
-using Blueprint41.TypeConversion;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blueprint41")]
+[assembly: AssemblyTitle("Blueprint41 Neo4j Driver v3")]
 [assembly: AssemblyDescription("An Object Graph Mapper for CSharp and Neo4j. It has support for defining the object model as a schema. It support refactor scripts written in CSharp, which you can add to your project. When you run your program and the graph is of an older version, the upgrade script will automatically be executed against the graph. It also support generation of type-safe data objects, so you know at compile time if your code is compatible with the latest upgrades.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CIRCLES ARROWS LIMITED")]
-[assembly: AssemblyProduct("Blueprint41")]
+[assembly: AssemblyProduct("Blueprint41 Neo4j Driver v3")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,8 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c2e15838-2187-461f-aa9a-d00f0123f9c2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -36,20 +32,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.18.0")]
 [assembly: AssemblyFileVersion("1.0.18.0")]
-
-[assembly: InternalsVisibleTo("Blueprint41.Modeller.Compare")]
-[assembly: InternalsVisibleTo("Blueprint41.Neo4jDriver.v3")]
-[assembly: InternalsVisibleTo("Blueprint41.Neo4jDriver.v4")]
-
-#region Type Conversions
-
-[assembly: Conversion(typeof(DecimalToLong))]
-[assembly: Conversion(typeof(LongToDecimal))]
-[assembly: Conversion(typeof(DateTimeToLong))]
-[assembly: Conversion(typeof(LongToDateTime))]
-[assembly: Conversion(typeof(GuidToString))]
-[assembly: Conversion(typeof(StringToGuid))]
-
-// Auto generated converters can be found in: ... 
-
-#endregion
