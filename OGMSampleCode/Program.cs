bc45dc522e779cf3634fcf09e194af71b275a5ca
Agent Smith <agent.smith@blueprint41.com>
2023-11-06 13:28:47 +0800

    fix adventureWorks sln

--- OGMSampleCode/Program.cs
+++ OGMSampleCode/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 using Blueprint41;
 using Blueprint41.Core;
 
 using Domain.Data.Manipulation;
+using Domain.Data.Query;
 
-using MemgraphTestApp;
-
-using neo4j = Blueprint41.Neo4j.Persistence.Driver.Memgraph;
+using neo4j = Blueprint41.Neo4j.Persistence.Void;
 
 namespace OGMSampleCode
 {
@@ -17,99 +14,75 @@ namespace OGMSampleCode
     {
         static void Main(string[] args)
         {
-            PersistenceProvider.CurrentPersistenceProvider = new neo4j.Neo4jPersistenceProvider($"bolt://localhost:7690", $"neo4j", $"passionite?01");
+            PersistenceProvider.CurrentPersistenceProvider = new neo4j.Neo4jPersistenceProvider($"bolt://localhost:7689", $"neo4j", $"neo");
 
             // The definition of what you can store in the graph can be found in the "Datastore" project
             // The type-safe objects you can program against to do CRUD operations can be found in the "Datastore.Generated" project
-            var model = new HumanResources
+            var model = new Datastore.AdventureWorks
             {
                 LogToConsole = true,
                 LogToDebugger = false,
             };
+
             model.Execute(true);
-            //CreateEntities();
-            UpdateEntities();
-        }
 
-        private static void UpdateEntities()
-        {
-            using (Transaction.Begin(true))
-            {
-                // create 2 departments in the graph
-                Department development = Department.Load("DEP_AAAAA");
-                Department support = Department.Load("DEP_BBBBB");
-
-                // and an employee
-                Employee employee = Employee.Load("EMP_AAAAA");
-                Employee employee2 = Employee.Load("EMP_BBBBB");
-
-                //employee.LastModifiedOn = Transaction.Current.TransactionDate;
-                employee.Department = support;
-                //employee.LastModifiedOn = Transaction.Current.TransactionDate;
-                employee2.Department = support;
-                //employee.Department = support;
-                //support.Employees.AddRange(new List<Employee>() { employee, employee2 });
-
-                // save them, since we did it right...
-                Transaction.Commit();
-                //Transaction.Rollback();
-            }
-        }
-        private static void CreateEntities()
-        {
             using (Transaction.Begin())
             {
                 // create 2 departments in the graph
                 Department development = new Department()
                 {
-                    Uid = "DEP_AAAAA",
                     Name = "Software Development",
-                    LastModifiedOn = Transaction.Current.TransactionDate
+                    GroupName = "IT",
                 };
                 Department support = new Department()
                 {
-                    Uid = "DEP_BBBBB",
                     Name = "Support",
-                    LastModifiedOn = Transaction.Current.TransactionDate
+                    GroupName = "IT",
                 };
 
                 // and an employee
-                Employee employee1 = new Employee()
+                Employee employee = new Employee()
                 {
-                    Uid = "EMP_AAAAA",
-                    FirstName = "Bong",
-                    LastName = "Revilla",
-                    LastModifiedOn = Transaction.Current.TransactionDate
+                    NationalIDNumber = "123",
+                    rowguid = Guid.NewGuid().ToString(),
+                    JobTitle = "Developer",
+                    BirthDate = new DateTime(1980, 1, 1),
+                    MaritalStatus = "M",
+                    Gender = "M",
+                    SalariedFlag = true,
+                    VacationHours = 0,
+                    SickLeaveHours = 0,
+                    Currentflag = true,
+                    ModifiedDate = Transaction.Current.TransactionDate,
                 };
-                development.Employees.Add(employee1);
-                Employee employee2 = new Employee()
-                {
-                    Uid = "EMP_BBBBB",
-                    FirstName = "Juan",
-                    LastName = "Enrile",
-                    LastModifiedOn = Transaction.Current.TransactionDate
-                };
-                development.Employees.Add(employee2);
-                Employee employee3 = new Employee()
-                {
-                    Uid = "EMP_CCCCC",
-                    FirstName = "Bato",
-                    LastName = "Stone",
-                    LastModifiedOn = Transaction.Current.TransactionDate
-                };
-                development.Employees.Add(employee3);
-                Employee employee4 = new Employee()
-                {
-                    Uid = "EMP_DDDDD",
-                    FirstName = "Bong",
-                    LastName = "Go",
-                    LastModifiedOn = Transaction.Current.TransactionDate
-                };
-                development.Employees.Add(employee4);
+                development.Employees.Add(employee);
 
                 // save them, since we did it right...
                 Transaction.Commit();
                 //Transaction.Rollback();
+            }
+
+            using (Transaction.Begin())
+            {
+                // Reusable type-safe query
+                var compiled = Transaction.CompiledQuery
+                    .Match(Node.Department.Alias(out var d).In.DEPARTMENT_CONTAINS_EMPLOYEE.Out.Employee.Alias(out var e))
+                    .Where(d.Name == Parameter.New<string>("DepartmentName"))
+                    .Return(d.Name.As("Department"), e.SickLeaveHours.Sum().As("TotalSickleaveHours"), e.VacationHours.Sum().As("TotalVacationHours"))
+                    .Compile();
+
+                // Make an execution context, which will hold the parameter values to be used
+                var context = compiled.GetExecutionContext();
+                context.SetParameter("DepartmentName", "Software Development");
+
+                // Execute query and display results
+                var resultSet = context.Execute();
+                foreach (var result in resultSet)
+                {
+                    Console.WriteLine($"Department: {result.Department}, Total outstanding sick leave hours: {result.TotalSickleaveHours}, Total outstanding vacation hours: {result.TotalVacationHours}.");
+                }
+
+                // No commit needed, since we're only reading anyway...
             }
         }
     }
