84f51fa8302a0286593b64e2aff87ee423cdbdd3
Agent Smith <agent.smith@blueprint41.com>
2024-01-22 13:27:05 +0800

    refactor parameter parsing

--- MovieGraph/MovieGraph/Blueprint41.Build/Program.cs
+++ MovieGraph/MovieGraph/Blueprint41.Build/Program.cs
@@ -8,91 +8,49 @@ namespace Blueprint41.Build
 {
     public static class Generator
     {
-        private const string MODEL_PATH_ARG = "--modelPath=";
-        private const string GENERATE_PATH_ARG = "--generatePath=";
-        private const string NAMESPACE_ARG = "--namespace=";
+        private const string MODEL_PATH_ARG = "modelPath";
+        private const string GENERATE_PATH_ARG = "generatePath";
+        private const string NAMESPACE_ARG = "namespace";
 
         public static void Main(string[] args)
         {
-            bool hasCommandlineArgumentErrors = false;
-            Dictionary<string, string> arguments = args.Select(item => item.Split('=')).ToDictionary(item => item[0].TrimStart('-', '/', ' ').TrimEnd(' ').ToLowerInvariant(), item => (item.Length == 1) ? item[0].Trim(' ', '"').ToLowerInvariant() : string.Join("=", item.Skip(1)).Trim(' ', '"'));
+            Dictionary<string, string> parameters = ParseParameters(args);
 
-            if (Exists("?") || Exists("help"))
+            string modelPath = null;
+            string generatePath = null;
+            string namespaceName = parameters.GetValueOrDefault(NAMESPACE_ARG, "Datastore");
+
+            if (parameters.TryGetValue(MODEL_PATH_ARG, out string modelPathValue))
+                modelPath = Path.GetFullPath(modelPathValue);
+
+            if (parameters.TryGetValue(GENERATE_PATH_ARG, out string generatePathValue))
+                generatePath = Path.GetFullPath(generatePathValue);
+
+            if (modelPath == null || generatePath == null)
             {
-                Console.WriteLine("-help    Displays command line argument information");
-                Console.WriteLine(MODEL_PATH_ARG);
-                Console.WriteLine(GENERATE_PATH_ARG);
-                Console.WriteLine(NAMESPACE_ARG);
+                Console.WriteLine($"Please provide both {MODEL_PATH_ARG} and {GENERATE_PATH_ARG} arguments for the file path.");
+                throw new InvalidOperationException($"Please provide both {MODEL_PATH_ARG} and {GENERATE_PATH_ARG} arguments for the file path.");
             }
-            else
+
+
+            Console.WriteLine("Generate Task Starting...");
+            Console.WriteLine($"ModelPath: '{modelPath}'");
+            Console.WriteLine($"GeneratePath: '{generatePath}'");
+            Console.WriteLine($"Namespace: '{namespaceName}'");
+            try
             {
-                string modelPath = null;
-                string generatePath = null;
-                string namespaceName = "Datastore";
-
-                foreach (var arg in args)
-                {
-                    if (arg.StartsWith(MODEL_PATH_ARG))
-                    {
-                        modelPath = Path.GetFullPath(arg[MODEL_PATH_ARG.Length..]);
-                    }
-                    else if (arg.StartsWith(GENERATE_PATH_ARG))
-                    {
-                        generatePath = Path.GetFullPath(arg[GENERATE_PATH_ARG.Length..]);
-                    }
-                    else if (arg.StartsWith(NAMESPACE_ARG))
-                    {
-                        namespaceName = arg[NAMESPACE_ARG.Length..];
-                    }
-                }
-                if (modelPath == null || generatePath == null)
-                {
-                    Console.WriteLine($"Please provide both {MODEL_PATH_ARG} and {GENERATE_PATH_ARG} arguments for the file path.");
-                    hasCommandlineArgumentErrors = true;
-                }
-
-                if (!hasCommandlineArgumentErrors)
-                {
-                    Console.WriteLine("Generate Task Starting...");
-                    Console.WriteLine($"ModelPath: '{modelPath}'");
-                    Console.WriteLine($"GeneratePath: '{generatePath}'");
-                    Console.WriteLine($"Namespace: '{namespaceName}'");
-                    try
-                    {
-                        Generate(modelPath, generatePath, namespaceName);
-                        Console.WriteLine("Generate Task Complete");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.Error.WriteLine($"Generate Task Failed.");
-                        Console.Error.WriteLine(ex.Message);
-                        throw;
-                    }
-                }
+                Generate(modelPath, generatePath, namespaceName);
+                Console.WriteLine("Generate Task Complete");
             }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Generate Task Failed.");
+                Console.Error.WriteLine(ex.Message);
+                throw;
+            }
+
             Console.WriteLine("Generate Task Exiting.");
             Environment.Exit(0);
-
-            string GetMandatory(string argumentName)
-            {
-                string value = GetOptional(argumentName, null);
-                if (value is null)
-                {
-                    Console.Error.WriteLine($"Argument '{argumentName}' is mandatory.");
-                    hasCommandlineArgumentErrors = true;
-                }
-
-                return value;
-            }
-            string GetOptional(string argumentName, string defaultValue)
-            {
-                return arguments.GetValueOrDefault(argumentName.ToLowerInvariant(), defaultValue);
-            }
-            bool Exists(string argumentName)
-            {
-                string value = GetOptional(argumentName, null);
-                return value is not null;
-            }
         }
         public static void Generate(string modelDllPath, string generatePath, string namespaceName)
         {
@@ -145,6 +103,25 @@ namespace Blueprint41.Build
                 types = ex.Types.Where(type => type is not null).ToArray()!;
             }
             return types;
+        }
+        static Dictionary<string, string> ParseParameters(string[] args)
+        {
+            var parameters = new Dictionary<string, string>();
+
+            foreach (string arg in args)
+            {
+                string[] parts = arg.Split('=');
+
+                if (parts.Length == 2)
+                {
+                    string key = parts[0].TrimStart('-').TrimStart('-');
+                    string value = parts[1].Trim('"');
+
+                    parameters[key] = value;
+                }
+            }
+
+            return parameters;
         }
     }
 }
