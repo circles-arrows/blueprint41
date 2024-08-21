d2852b3532d37ee46ddbe42994ba99b8b41c3d56
rkarman <31648959+circles-arrows@users.noreply.github.com>
2024-08-21 18:17:23 +0800

    Added mock model

--- Version 2/Laboratory version 2/Program.cs
+++ Version 2/Laboratory version 2/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+using Blueprint41.Core;
+using DataStore;
+
 namespace Laboratory_version_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PersistenceProvider connection = PersistenceProvider.VoidProvider; // new PersistenceProvider("localhost", 5687, "neo4j", "neoneoneo", "unittest");
+
+            MockModel model = new MockModel(connection);
+            model.Execute(false);
+
+            // Generate documentation
         }
     }
 }
